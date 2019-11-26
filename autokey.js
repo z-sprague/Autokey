8eb177dbf282fe1bac98fd24bde2a2a3ac4cd79f
z-sprague <32646325+z-sprague@users.noreply.github.com>
2019-11-26 18:30:37 -0500

    Update Functions

--- autokey.js
+++ autokey.js
@@ -21,14 +21,6 @@ function makeTable(){
 			table[i][j] = String.fromCharCode(let);
 		}
 	}
-/*
-	for (var i = 0; i < 26; i++){
-		for (var j = 0; j < 26; j++){
-			document.write(table[i][j] + " ");
-    }
-    document.write("<br>");
-}
-*/
 }
 
 /* Encipher functions by extending plain text */
@@ -39,16 +31,16 @@ function encipher_Clicked(){
 
 	/* Error checking for empty input fields */
 
-	if (document.getElementById("radioPlain").checked){
-		var cryptText = extendPlain_encipher(keyword, input);
-		cryptText = output(cryptText);
-		cText.value = cryptText;
-	}
-	else{
-		var cryptText = extendCrypt_encipher(key, input);
-		cryptText = output(cryptText);
-		cText.value = cryptText;
-	}
+		if (document.getElementById("radioPlain").checked){
+			var cryptText = extendPlain_encipher(keyword, input);
+			cryptText = output(cryptText);
+			cText.value = cryptText;
+		}
+		else{
+			var cryptText = extendCrypt_encipher(key, input);
+			cryptText = output(cryptText);
+			cText.value = cryptText;
+		}
 }
 
 function extendPlain_encipher(keyword, plainText){
@@ -58,45 +50,11 @@ function extendPlain_encipher(keyword, plainText){
 	for (var i = 0; i < len; i++){
 		var keyLetter = keyword.charCodeAt(i);
 		var pText = plainText.charCodeAt(i);
-    var row = keyLetter - 65;
-    var col = pText - 65;
+		var row = keyLetter - 65;
+		var col = pText - 65;
 		cryptText = cryptText + table[row][col];
 	}
 	return cryptText;
-}
-
-function extendCrypt_encipher(key, plainText){
-	var len = plainText.length;
-	var keyword = key + plainText;
-	var cText = extendPlain_encipher(keyword, plainText);
-	var extend = key + cText;
-	var cryptText = "";
-
-	for (var i = 0; i < len; i++){
-		var keyLetter = extend.charCodeAt(i);
-		var pText = plainText.charCodeAt(i);
-    var row = keyLetter - 65;
-    var col = pText - 65;
-		cryptText = cryptText + table[row][col];
-	}
-	return cryptText;
-}
-
-/* Decipher functions by extending by plain text or cipher text depending on the radio button selection */
-function decipher_Clicked(){
-	var cText = document.getElementById("cText").value.replace(/[^A-Za-z]+/g, "").toUpperCase();
-	var key = document.getElementById("keyword").value.replace(/[^A-Za-z]+/g, "").toUpperCase();
-
-	if (document.getElementById("radioPlain").checked){
-		var plainText = extendPlain_decipher(key, cText);
-		plainText = output(plainText);
-		pText.value = plainText.toLowerCase();
-	}
-	else{
-		var plainText = extendCrypt_decipher(key, cText);
-		plainText = output(plainText);
-		cText.value = plainText;
-	}
 }
 
 function extendPlain_decipher(key, cText){
@@ -115,8 +73,57 @@ function extendPlain_decipher(key, cText){
 	return plainMsg;
 }
 
-function extendCrypt_decipher(key, cText){
+/* Decipher functions by extending by plain text or cipher text depending on the radio button selection */
+function decipher_Clicked(){
+	var cText = document.getElementById("cText").value.replace(/[^A-Za-z]+/g, "").toUpperCase();
+	var key = document.getElementById("keyword").value.replace(/[^A-Za-z]+/g, "").toUpperCase();
 
+	if (document.getElementById("radioPlain").checked){
+		var plainText = extendPlain_decipher(key, cText);
+		plainText = output(plainText);
+		pText.value = plainText.toLowerCase();
+	}
+	else{
+		var plainText = extendCrypt_decipher(key, cText);
+		plainText = output(plainText);
+		pText.value = plainText.toLowerCase();
+	}
+}
+
+function extendCrypt_encipher(key, plainText){
+	var len = plainText.length;
+	var keyword = key + plainText;
+	var cText = extendPlain_encipher(keyword, plainText);
+	var extend = key;
+	var cryptText = "";
+
+	for (var i = 0; i < len; i++){
+		var keyLetter = extend.charCodeAt(i);
+		var pText = plainText.charCodeAt(i);
+		var row = keyLetter - 65;
+		var col = pText - 65;
+		cryptText = cryptText + table[row][col];
+		extend += table[row][col];
+	}
+	return cryptText;
+}
+
+function extendCrypt_decipher(key, cText){
+	var len = cText.length;
+	var keyword = key + cText;
+	var plainMsg = "";
+	var current = key;
+	
+	for (var i = 0; i < len; i++){
+		var keyLetter = alpha.indexOf(keyword.charAt(i));
+		var cryptLet = alpha.indexOf(cText.charAt(i));
+		var total = (cryptLet - keyLetter) % 26;
+		total = (total < 0) ? total + 26 : total;
+		plainMsg += alpha.charAt(total);
+		current += alpha.charAt(total);
+	}
+	return plainMsg;
+	
 }
 
 /*
@@ -132,10 +139,11 @@ function clear_Clicked(){
     document.getElementById("cText").value = "";
 }
 
+/* Output text in 5-letter blocks */
 function output(text){
 	var blockText = "";
 	for (var i = 0; i < text.length; i++){
-		if (i % 5 === 0 && i !== 0)
+		if (i % 5 == 0 && i != 0)
 			blockText += " " + text.charAt(i);
 		else
 			blockText += text.charAt(i);
